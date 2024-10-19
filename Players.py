3afd8968311fee7dedf535a88c1e939c082a0487
Jesus Perello <jfperello@gmail.com>
2024-10-19 17:36:43 -0600

    Added init and catch rate methods to Player class

--- Players.py
+++ Players.py
@@ -1,6 +1,10 @@
-class my_class(object):
-    pass
+class Player:
 
+    def __init__(self, name, pos, catches, targets):
+        self.name = name
+        self.pos = pos
+        self.catches = catches
+        self.targets = targets
 
-
-
+    def catch_rate(self):
+        return self.catches/self.targets
